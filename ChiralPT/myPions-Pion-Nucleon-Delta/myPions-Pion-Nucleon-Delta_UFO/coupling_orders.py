f7e0fcea485cd58700e5c61b19f6d56c05550a28
Aircondition <misa.toman97@gmail.com>
2025-10-07 13:31:43 -0700

    Regenerated the UFO files with a proper name

---
+++ ChiralPT/myPions-Pion-Nucleon-Delta/myPions-Pion-Nucleon-Delta_UFO/coupling_orders.py
@@ -0,0 +1,12 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 14.0.0 for Microsoft Windows (64-bit) (December 13, 2023)
+# Date: Tue 7 Oct 2025 13:28:49
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
