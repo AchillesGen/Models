78deb5333cba8b8a9193900bc04ee821a8aefc15
Josh Isaacson <isaacs21@msu.edu>
2025-12-04 11:59:10 -0500

    feat: Add python3 version of ChiPT UFO files

---
+++ ChiralPT/myPions-Pion-Nucleon-Delta-GI-coupling/ChiPT_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 14.3.0 for Linux x86 (64-bit) (July 8, 2025)
+# Date: Wed 29 Oct 2025 00:14:06
+
+
+from .object_library import all_couplings, Coupling
+
+from .function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ ChiralPT/myPions-Pion-Nucleon-Delta-GI-coupling/ChiPT_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 14.3.0 for Linux x86 (64-bit) (July 8, 2025)
+# Date: Wed 29 Oct 2025 00:14:06
+
+
+from .object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 0,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
