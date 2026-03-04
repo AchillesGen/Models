50e63542b3542e000b00646e8f9715708d0d202e
Mareen Hoppe <mareen.hoppe@mailbox.tu-dresden.de>
2026-03-04 11:06:42 +0100

    Fixed UFO model with Photo excitation

---
+++ ChiralPT/myPions-Pion-Nucleon-Delta-GI-coupling-Photoexcitation/myPions_Pion_Nucleon_Delta_GI_coupling_Photoexcitation_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 13.3.1 for Linux x86 (64-bit) (July 24, 2023)
+# Date: Wed 4 Mar 2026 10:58:36
+
+
+from .object_library import all_orders, CouplingOrder
+
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 0,
+                    hierarchy = 1)
+
